79b5a97b1b8a7e9cb4d39a77fa557b746d615be2
KhizerAhmed1 <khizerahmed004@gmail.com>
2025-05-25 20:24:15 +0500

    init commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,14 +4,76 @@ package org.example;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello and welcome!");
+        Car C1 = new Car("SuperStar", "Civic", 2022, 4);
+        C1.displayInfo();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Bike B1 = new Bike("Honda", "Happy", 2024, "Sports");
+        B1.displayInfo();
+
+        Truck T1 = new Truck("Ford-FSeries", "BeSpoke", 2025, 20.5);
+        T1.displayInfo();
     }
 }
+
+class Vehicle {
+    String Make;
+    String Model;
+    int Year;
+
+    Vehicle(String Make, String Model, int Year) {
+        this.Make = Make;
+        this.Model = Model;
+        this.Year = Year;
+    }
+}
+
+class Car extends Vehicle {
+    int Doors;
+
+    Car(String Make, String Model, int Year, int Doors) {
+        super(Make, Model, Year);
+        this.Doors = Doors;
+    }
+
+    void displayInfo() {
+        System.out.println("Make: " + Make);
+        System.out.println("Model: " + Model);
+        System.out.println("Year: " + Year);
+        System.out.println("Doors: " + Doors);
+    }
+}
+
+class Bike extends Vehicle {
+    String Type;
+
+    Bike(String Make, String Model, int Year, String Type) {
+        super(Make, Model, Year);
+        this.Type = Type;
+    }
+
+    void displayInfo() {
+        System.out.println("Make: " + Make);
+        System.out.println("Model: " + Model);
+        System.out.println("Year: " + Year);
+        System.out.println("Type: " + Type);
+    }
+}
+
+class Truck extends Vehicle {
+    double weight;
+
+    Truck(String Make, String Model, int Year, double weight) {
+        super(Make, Model, Year);
+        this.weight = weight;
+    }
+
+    void displayInfo() {
+        System.out.println("Make: " + Make);
+        System.out.println("Model: " + Model);
+        System.out.println("Year: " + Year);
+        System.out.println("Weight: " + weight + " tons");
+    }
+}
+
+
+
